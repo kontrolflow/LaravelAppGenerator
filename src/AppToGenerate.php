98ea9d3b997c5e202fe1e71eed6c1300ba897577
Jullian Abinsay <jullianabinsay@gmail.com>
2020-03-29 23:04:51 +0000

    Creates sandbox for generated files, Adds AppToGenerate Class, and inserts code for file modification.

---
+++ src/AppToGenerate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace kontrolflow\LaravelAppGenerator;
+
+class AppToGenerate
+{
+    var $appName = "ToolDirectory";
+
+    var $appRoot = '/var/www/laravelApp/app/LaravelAppGenerator/sandbox';
+
+    var $modelName = 'Tool';
+
+    public static function init() {
+
+        echo "Hello World";
+
+    }
+
+}
--- src/LaravelAppGenerator.php
+++ src/LaravelAppGenerator.php
@@ -4,11 +4,32 @@ namespace kontrolflow\LaravelAppGenerator;
 
 class LaravelAppGenerator
 {
-	
+
 	public static function test()
 	{
 		echo 'Hello World';
 	}
-	
-	
+
+    public static function init() {
+
+        $app = new AppToGenerate;
+        $app->init();
+
+
+        $file = '/var/www/laravelApp/app/LaravelAppGenerator/sandbox/test.txt';
+
+        // Open the file to get existing content
+        $current = file_get_contents($file);
+
+        // Append a new person to the file
+        $current .= "John Smith\n";
+
+        echo $current;
+
+        // Write the contents back to the file
+        //file_put_contents($file, $current);
+
+    }
+
+
 }
